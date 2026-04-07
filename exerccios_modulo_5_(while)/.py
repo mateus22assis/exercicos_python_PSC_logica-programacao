c4ee7c475d2975e7d14c1a9e52fab73bec018b36
mateus22assis <mateus.22assis@gmail.com>
2026-04-07 13:20:19 -0300

    terminando lista do modulo 05 while

---
+++ exerccios_modulo_5_(while)/.py
@@ -0,0 +1,18 @@
+cont_idade30_40 = 0
+soma = 0
+qtde_pessoas = 0
+idade = int(input("Digite uma idade: "))
+while idade >= 0:
+  qtde_pessoas += 1
+  soma += idade
+  if idade >= 20 and idade <= 40:
+    cont_idade30_40 += 1
+  idade = int(input("Digite uma idade: "))
+
+
+media = soma / qtde_pessoas
+
+print("soma: ", soma)
+print("qtde: ", qtde_pessoas)
+print("media: ", media)
+print("exite(m)", cont_idade30_40, "pessoa(s) entre 20 e 40 anos")
